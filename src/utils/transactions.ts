14e7c3b06cbad6add89cf4582cf6d07f4ea627e2
Joel Thorstensson <joel@stigmergic.org>
2025-03-13 16:23:48 +0000

    Allow sending batch creation

--- src/utils/transactions.ts
+++ src/utils/transactions.ts
@@ -11,6 +11,7 @@ import {
   type TransactionSummary,
   TransactionInfoType,
   DetailedExecutionInfoType,
+  getTransactionDetails,
 } from '@safe-global/safe-gateway-typescript-sdk'
 import { ConflictType, TransactionListItemType } from '@safe-global/safe-gateway-typescript-sdk'
 import {
@@ -142,6 +143,14 @@ export const getMultiSendTxs = async (
       }
     })
     .filter(Boolean) as MetaTransactionData[]
+}
+
+export const getTxsWithDetails = (txs: Transaction[], chainId: string) => {
+  return Promise.all(
+    txs.map(async (tx) => {
+      return await getTransactionDetails(chainId, tx.transaction.id)
+    }),
+  )
 }
 
 export const getTxOptions = (params: AdvancedParameters, currentChain: ChainInfo | undefined): TransactionOptions => {
